282d295009c73c0372d708f8f6bf343b60432bab
Rimfat1234 <nansohkumkurrimfat@gmail.com>
2025-06-26 14:46:55 +0100

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const user = {
+  username: "NANSOH_KUMKUR",
+  password: "nansohkumkur@1234"
+};
+const database = [user];
+const newsfeed = [
+  {
+    username: "RIMFAT",
+    timeline: "Just ate lunch!"
+  },
+  {
+    username: "KUMKUR",
+    timeline: "Learning JavaScript is fun!"
+  },
+  {
+    username: "NANSOH",
+    timeline: "Going on vacation next week."
+  }
+];
+console.log("User object:", user);
+console.log("Database array:", database);
+console.log("Newsfeed array:", newsfeed);
