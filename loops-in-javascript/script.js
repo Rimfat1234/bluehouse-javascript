80e12487318e61faa789262d0905a826e4e0cd60
Rimfat1234 <nansohkumkurrimfat@gmail.com>
2025-07-08 12:35:39 +0100

    update

---
+++ loops-in-javascript/script.js
@@ -0,0 +1,20 @@
+for(let i=1; i<=10; i++){
+    if(i % 2 ===0)
+    {
+        console.log(i);
+    }
+};
+
+let count = 10
+while(count>=1){
+    console.log(count);
+    count--
+};
+
+
+var x=5
+do{
+    console.log(x);
+} while(x=5);
+
+
